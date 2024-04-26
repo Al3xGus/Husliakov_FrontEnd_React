9128e6bbacf174702154f29a3b07d30ec15d6374
Al3xGus <sgura.guslyakov@gmail.com>
2024-04-26 18:08:44 +0300

    To_do_list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import EmojiList from './EmojiList';
+import TodoList from './TodoList';
 
 function App() {
     return (
         <div className="App">
-            <EmojiList />
+            <TodoList />
         </div>
     );
 }
